e78fcabdf3b4eb9b240d31a6c98ee6c7e5e14fd0
dusanvuckovic86 <dusan_vuckovic@live.com>
2022-01-31 15:32:09 +0100

    Kreiranje klase Chatroom

---
+++ JAVASCRIPT/27_CHAT_PROJEKAT/chat.js
@@ -0,0 +1,25 @@
+class Chatroom {
+    constructor(r, u) {
+        this.room = r;
+        this.username = u;
+        this.chats = db.collection("chats");
+    }
+
+    set room(r) {
+        this._room = r;
+    }
+    get room() {
+        return this._room;
+    }
+
+    set username(u) {
+        this._username = u;
+    }
+    get username() {
+        return this._username;
+    }
+
+
+}
+
+export default Chatroom;
---
+++ JAVASCRIPT/27_CHAT_PROJEKAT/script.js
@@ -0,0 +1,9 @@
+import Chatroom from "./chat.js";
+
+let chat1 = new Chatroom ("general","pPeric" )
+
+console.log(chat1)
+chat1.username = "test"
+console.log(chat1.username)
+chat1.room = "testiramo"
+console.log(chat1.room)
